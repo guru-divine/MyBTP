89194cef12e7ae055527c9e3b195ce6e42cfa690
Divya Raj <divya.raj@kgpian.iitkgp.ac.in>
2024-05-16 00:02:50 +0530

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, redirect, render_template, render_template_string, jsonify
+from flask.helpers import url_for
+from dotenv import load_dotenv
+from collections import Counter
+
+import os
+import json, requests
+
+app = Flask("DCODE")
+app.secret_key = "harshu"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+app.run(debug=True)
