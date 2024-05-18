75ade50c140c9cb5d406f0853d5e0dc828abf33c
Divya Raj <divya.raj@kgpian.iitkgp.ac.in>
2024-05-18 21:12:38 +0530

    Completed profs personalized website

--- backend/main.py
+++ backend/main.py
@@ -6,16 +6,42 @@ from collections import Counter
 import os
 import json, requests
 
-app = Flask("DCODE")
+app = Flask("MyBTP")
 app.secret_key = "harshu"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/user-profile")
+@app.route("/profile")
 def user_profile():
     return render_template("users-profile.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/message")
+def message():
+    return render_template("register.html")
+
+@app.route("/notification")
+def notification():
+    return render_template("register.html")
+
+@app.route("/setting")
+def setting():
+    return render_template("users-profile.html")
+
+
+
+# Catch-all route for any other URL
+@app.route("/<path:path>")
+def catch_all(path):
+    return render_template("pages-error-404.html")
 
 app.run(debug=True)
